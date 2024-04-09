9b647d045e7dd3c6d96236862589920c75190ebf
evanteal15 <evanteal@umich.edu>
2024-04-09 15:34:40 -0400

    parsing stuff

--- parse.cpp
+++ parse.cpp
@@ -5,17 +5,20 @@
 
 #include <getopt.h>
 
+#include <bits/stdc++.h>
+
 #include "ctype.h"
 using namespace std;
 
 
 int main(int argc, char* argv[]) {
-    string word;
+    string line;
     unordered_map<string, int> count;
     //! \n isn't treated as readible sequence
     //! Some punctuation gets read into the same string as words
-    while (cin >> word) {
-        if (isupper(word[0])) {
+    while (getline(cin, line)) {
+        stringstream st(line);
+        if (isupper([0])) {
             cout << static_cast<char>(tolower(word[0])) << " ";
         }
         cout << word << " ";
