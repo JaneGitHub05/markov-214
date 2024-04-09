57a5d59d822c3bf1cac39540475c78c139aa6cff
Jane Wei <janewei@umich.edu>
2024-04-08 20:09:55 -0400

    Added sample text

--- parse.cpp
+++ parse.cpp
@@ -1,1 +1,15 @@
+#include <algorithm>
+#include <fstream>
+#include <getopt.h>
+#include <iostream>
+using namespace std;
+
 // TODO: Parse contents of input file
+
+int main(int argc, char *argv[]) {
+  string word;
+  while (cin >> word) {
+    cout << word;
+  }
+  return 0;
+}
