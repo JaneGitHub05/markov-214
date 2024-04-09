8a4cd2380ef483abc0db963cddee2663dc89c566
evanteal15 <evanteal@umich.edu>
2024-04-09 16:06:52 -0400

    working parsing algorithm

--- parse.cpp
+++ parse.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cctype>
 #include <cstring>
 #include <fstream>
 #include <iostream>
@@ -8,19 +9,29 @@
 
 #include <bits/stdc++.h>
 
-#include "ctype.h"
 using namespace std;
 
 
 int main(int argc, char* argv[]) {
     string line;
-    unordered_map<string, int> count;
+    unordered_map<string, vector<string>> probs;
     //! \n isn't treated as readible sequence
     //! Some punctuation gets read into the same string as words
-    while (getline(cin, line)) {
-        stringstream st(tolower(line));
+    while (getline(cin, line, '\n')) {
+        stringstream st(line);
         string word;
-        while (getline(st, word, ' ')) {}
+        while (getline(st, word, ' ')) {
+            string onlychars;
+            for (int i = 0; i <= static_cast<int>(word.length()); i++) {
+                if (isalpha(word[i]) != 0) {
+                    onlychars += tolower(word[i]);
+                }
+            }
+            if (onlychars.length() > 0) {
+                cout << onlychars << " ";
+            }
+        }
+        cout << "\n";
     }
     return 0;
 }
