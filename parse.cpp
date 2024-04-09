58f6d1624e471de6c69cb5a0bf9ba48ab5154002
evanteal15 <evanteal@umich.edu>
2024-04-09 15:10:33 -0400

    Deleted dumb comment

--- parse.cpp
+++ parse.cpp
@@ -1,30 +1,31 @@
-#include "ctype.h"
 #include <algorithm>
 #include <fstream>
-#include <getopt.h>
 #include <iostream>
 #include <map>
+
+#include <getopt.h>
+
+#include "ctype.h"
 using namespace std;
 
-// TODO: Parse contents of input file
 
-int main(int argc, char *argv[]) {
-  string word;
-  unordered_map<string, int> count;
-  //! \n isn't treated as readible sequence
-  //! Some punctuation gets read into the same string as words
-  while (cin >> word) {
-    if (isupper(word[0])) {
-      cout << static_cast<char>(tolower(word[0])) << " ";
+int main(int argc, char* argv[]) {
+    string word;
+    unordered_map<string, int> count;
+    //! \n isn't treated as readible sequence
+    //! Some punctuation gets read into the same string as words
+    while (cin >> word) {
+        if (isupper(word[0])) {
+            cout << static_cast<char>(tolower(word[0])) << " ";
+        }
+        cout << word << " ";
+        if (auto search = count.find(word); search != count.end()) {
+            count[word] += 1;
+            cout << word << ": " << count[word] << "\n";
+        } else {
+            count[word] = 1;
+        }
+        cout << "\n";
     }
-    cout << word << " ";
-    if (auto search = count.find(word); search != count.end()) {
-      count[word] += 1;
-      cout << word << ": " << count[word] << "\n";
-    } else {
-      count[word] = 1;
-    }
-    cout << "\n";
-  }
-  return 0;
+    return 0;
 }
