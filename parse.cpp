6855471613f52c0b24c5595f9d8a4c214347ceb9
evanteal15 <evanteal@umich.edu>
2024-04-09 17:08:07 -0400

    working parsing algorithm

--- parse.cpp
+++ parse.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <cctype>
 #include <cstring>
 #include <fstream>
@@ -15,10 +16,13 @@ using namespace std;
 int main(int argc, char* argv[]) {
     string line;
     unordered_map<string, vector<string>> probs;
+    vector<string> dict;
     //! \n isn't treated as readible sequence
     //! Some punctuation gets read into the same string as words
     string prev = "\n";
     probs["\n"];
+    dict[0] = "\n";
+    int count = 1;
     while (getline(cin, line, '\n')) {
         stringstream st(line);
         string word;
@@ -32,6 +36,9 @@ int main(int argc, char* argv[]) {
             if (onlychars.length() > 0) {
                 cout << onlychars << " ";
                 probs[onlychars];
+                if (dict.find(onlychars) == dict.end()) {
+                    dict[count++] = onlychars;
+                }
                 probs[prev].push_back(onlychars);
                 prev = onlychars;
             }
@@ -40,5 +47,13 @@ int main(int argc, char* argv[]) {
         probs[prev].emplace_back("\n");
         prev = "\n";
     }
+    int size = dict.size();
+    int transform[size][size];
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            probs[] cout << transform[i][j];
+        }
+    }
+
     return 0;
 }
