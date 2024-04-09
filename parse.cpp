afce51971a86a741f84bb9025a81fbba3bac1471
Jane Wei <janewei@umich.edu>
2024-04-08 20:39:37 -0400

    Added output file

--- parse.cpp
+++ parse.cpp
@@ -1,15 +1,29 @@
+#include "ctype.h"
 #include <algorithm>
 #include <fstream>
 #include <getopt.h>
 #include <iostream>
+#include <map>
 using namespace std;
 
 // TODO: Parse contents of input file
 
 int main(int argc, char *argv[]) {
   string word;
+  unordered_map<string, int> count;
+  //! \n isn't treated as readible sequence
   while (cin >> word) {
-    cout << word;
+    if (isupper(word[0])) {
+      cout << static_cast<char>(tolower(word[0])) << " ";
+    }
+    cout << word << " ";
+    if (auto search = count.find(word); search != count.end()) {
+      count[word] += 1;
+      cout << word << ": " << count[word] << "\n";
+    } else {
+      count[word] = 1;
+    }
+    cout << "\n";
   }
   return 0;
 }
