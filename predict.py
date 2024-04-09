c9b343daf1d7fd0efe47ccee15fac0ec605e57db
Jane Wei <janewei@umich.edu>
2024-04-09 17:12:02 -0400

    Added matrix code

---
+++ predict.py
@@ -0,0 +1,7 @@
+# GOAL: Predict what word comes after initial word after n steps + 
+
+# Functionality: 
+# 1. Take in transformation matrix and an input state
+# Algorithm 1: for-loop OR recursive
+
+# Algorithm 2: Diagonalize, then raise to power
