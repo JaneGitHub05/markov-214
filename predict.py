42d61a79c0234839090995ff180f63e408a30fea
Jane Wei <janewei@umich.edu>
2024-04-11 13:28:54 -0400

    Updated and added small test file for python predict algorithm

--- predict.py
+++ predict.py
@@ -1,14 +1,29 @@
-import numpy as np
-
 # GOAL: Generate line of n words given an initial word + track runtime between two solutions
 
-# Functionality: 
+######## Retrieve input #########
+
+# Get ordered list of words
+words = []
+line = input()
+for word in line.split():
+    words.append(word)
+    print(f"{word} ")
+# Record transition matrix
+rows, cols = (len(words), len(words))
+print(f"{rows} x {cols}")
+matrix = [rows][cols]
+while True:
+    try:
+        row = input()
+        for i in row.split():
+            val = i
+            print(f"{val} ")
+    except EOFError: # reached end of file
+        break
+
+######## Functionality ######## 
 # 1. Take in transformation matrix and an input state
 # 2. Predict resultant state after n steps using - 
-
-input("Enter transformation matrix")
-
-input("Enter initial state")
 
 # TODO: Solution 1: for-loop OR recursive
 
