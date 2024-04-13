2a7ec04584d18e8a5fb9bbe0c60a90b60b3badb3
Jane Wei <janewei@umich.edu>
2024-04-13 15:34:30 -0400

    Replaced #include bits/stdc++.h in parse.cpp with #include <sstream>, added capability to define initial state in parse.cpp, finished iterative algorithm in predict.py

--- parse.cpp
+++ parse.cpp
@@ -10,96 +10,98 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <bits/stdc++.h>
+#include <sstream>
 
 using namespace std;
 
+int main(int argc, char *argv[]) {
+  // input initial state as well, e.g. ./parse know 5 >
+  string initial = argv[1];
+  int num = atoi(argv[2]);
+  cout << initial << "\n" << num << " \n";
+  string line;
+  map<string, vector<string> > probs;
+  map<string, int> dict;
+  //! \n isn't treated as readible sequence
+  //! Some punctuation gets read into the same string as words
+  string prev = "\n";
+  probs["\n"];
+  dict["\n"] = 0;
+  int index = 1;
+  while (getline(cin, line, '\n')) {
+    stringstream st(line);
+    string word;
+    while (getline(st, word, ' ')) {
+      string onlychars;
+      for (int i = 0; i <= static_cast<int>(word.length()); i++) {
+        if (isalpha(word[i]) != 0) {
+          onlychars += tolower(word[i]);
+        }
+      }
+      if (onlychars.length() > 0) {
+        cout << onlychars << " ";
+        probs[onlychars];
+        if (dict.find(onlychars) == dict.end()) {
+          dict[onlychars] = 0;
+        }
+        probs[prev].push_back(onlychars);
+        prev = onlychars;
+      }
+    }
+    cout << "\n";
+    probs[prev].emplace_back("\n");
+    prev = "\n";
+  }
+  int size = dict.size();
+  double transform[size][size];
+  memset(transform, 0, size * size * sizeof(double));
+  int i2 = 0;
+  cout << "\n\n";
+  for (auto &d : dict) {
+    d.second = i2;
+    i2++;
+  }
+  i2 = 0;
+  for (auto &prob : probs) {
+    for (int j = 0; j < prob.second.size(); j++) {
+      transform[i2][dict[prob.second[j]]] +=
+          (static_cast<double>(1) / prob.second.size());
+    }
+    i2++;
+  }
 
-int main(int argc, char* argv[]) {
-    int num = atoi(argv[1]);
-    cout << num << " \n";
-    string line;
-    map<string, vector<string>> probs;
-    map<string, int> dict;
-    //! \n isn't treated as readible sequence
-    //! Some punctuation gets read into the same string as words
-    string prev = "\n";
-    probs["\n"];
-    dict["\n"] = 0;
-    int index = 1;
-    while (getline(cin, line, '\n')) {
-        stringstream st(line);
-        string word;
-        while (getline(st, word, ' ')) {
-            string onlychars;
-            for (int i = 0; i <= static_cast<int>(word.length()); i++) {
-                if (isalpha(word[i]) != 0) {
-                    onlychars += tolower(word[i]);
-                }
-            }
-            if (onlychars.length() > 0) {
-                cout << onlychars << " ";
-                probs[onlychars];
-                if (dict.find(onlychars) == dict.end()) {
-                    dict[onlychars] = 0;
-                }
-                probs[prev].push_back(onlychars);
-                prev = onlychars;
-            }
-        }
-        cout << "\n";
-        probs[prev].emplace_back("\n");
-        prev = "\n";
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      cout << transform[i][j] << " ";
     }
-    int size = dict.size();
-    double transform[size][size];
-    memset(transform, 0, size * size * sizeof(double));
-    int i2 = 0;
-    cout << "\n\n";
-    for (auto& d : dict) {
-        d.second = i2;
-        i2++;
-    }
-    i2 = 0;
-    for (auto& prob : probs) {
-        for (int j = 0; j < prob.second.size(); j++) {
-            transform[i2][dict[prob.second[j]]] += (static_cast<double>(1) / prob.second.size());
-        }
-        i2++;
-    }
+    cout << "\n";
+  }
 
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            cout << transform[i][j] << " ";
-        }
-        cout << "\n";
+  fstream fout;
+  fout.open("transformnew.csv", ios::out | ios::app);
+  fout << initial << "\n" << num << "\n";
+  int k = 0;
+  for (auto &i : dict) {
+    if (i.first[0] != '\n') {
+      fout << i.first;
+      if (k != size - 1) {
+        fout << ",";
+      }
     }
-
-    fstream fout;
-    fout.open("transformnew.csv", ios::out | ios::app);
-    fout << num << "\n";
-    int k = 0;
-    for (auto& i : dict) {
-        if (i.first[0] != '\n') {
-            fout << i.first;
-            if (k != size - 1) {
-                fout << ",";
-            }
-        }
-        k++;
+    k++;
+  }
+  fout << "\n";
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      fout << transform[i][j];
+      if (j != size - 1) {
+        fout << ",";
+      }
     }
     fout << "\n";
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            fout << transform[i][j];
-            if (j != size - 1) {
-                fout << ",";
-            }
-        }
-        fout << "\n";
-    }
-    remove("transform.csv");
-    rename("transformnew.csv", "transform.csv");
+  }
+  remove("transform.csv");
+  rename("transformnew.csv", "transform.csv");
 
-    return 0;
+  return 0;
 }
--- predict.py
+++ predict.py
@@ -1,47 +1,61 @@
 import numpy as np
 import pandas as pd
-# GOAL: Generate line of n words given an initial word + track runtime between two solutions
+# GOAL: Generate line of x words given an initial word + track runtime between two solutions
+#* THIS IS A FIRST-ORDER MODEL: only considers T(n-1) for T(n) - "memoryless" property of a stochastic process
 
-######## Retrieve input #########
-# Get user input (number of words to generate after a newline)
-x = input()
+######## Take in input #########
+# Get user input (initial word, and number of words to generate after it)
+initial = input()
+print(f"{initial}")
+x = int(input())
 print(f"{x}")
+# TODO: Get user input (method of predicting x words: it or diag)
 
 # Get ordered list of words
-words = []
+words = ["\n"]
 line = input()
 for word in line.split(','):
     words.append(word)
     # print(f"{word} ")
-# Record transition matrix
-n = len(words)
+# record transition matrix
+n = len(words) # + 1 if taking in csv, because csv doesn't include \n
 print(f"{n} x {n}")
 matrix = [[] for i in range(n)]
-# m2 = np.zeros((n, n))
+# npmatrix = np.zeros((n, n))
 
 for i in range(0, n):
     try:
         row = input()
         for val in row.split(','):
-            matrix[i].append(val)
+            matrix[i].append(float(val))
             # print(f"{val} ")
     except EOFError: # reached end of file
         break
 
-# m2 = matrix
-
-# for v in m2:
-#     print(f"{v}")
-
+######## Functionality ######## 
+#* 1. Get transformation matrix and an initial state
+# transpose matrix
 tmatrix = np.transpose(matrix)
 
-for v in tmatrix:
-    print(f"{v}")
+print(f"{tmatrix}")
 
-######## Functionality ######## 
-# 1. Take in transformation matrix and an input state
-# 2. Predict resultant state after n steps using - 
+initialState = np.array([0 for i in range(n)])
+initialState[words.index(initial)] = 1
+# transform input from row to column vector
+initialState.shape = (n, 1)
+# print(f"{input}")
 
+#* 2. Predict resultant state after n steps using - 
 # TODO: Solution 1: for-loop OR recursive
+nextState = initialState
+for i in range(x):
+    print(f"{nextState}")
+    nextState = np.matmul(tmatrix, nextState)
+    # transform nextState from nD into 1D array
+    nextState1D = np.ndarray.flatten(nextState)
+    print(f"{pd.Series(nextState1D).idxmax()}: {words[pd.Series(nextState1D).idxmax()]}")
+    nextWord = pd.Series(nextState1D).idxmax()
+    nextState = [0 for i in range(n)]
+    nextState[nextWord] = 1
 
 # TODO: Solution 2: Diagonalize, then raise to power
